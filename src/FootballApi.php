de1d6109309e3bd2ffd70ec386d1a9ce37777590
Qadeer Sipra <macbook@Qadeer-MacBook-Pro.local>
2024-06-13 11:26:20 +0500

    Teams End-points Added

--- src/FootballApi.php
+++ src/FootballApi.php
@@ -197,7 +197,7 @@ class FootballApi
     }
 
     /**
-     * nextXFixtures
+     * lasXFixtures
      * Although the data is updated every 15 seconds, depending on the competition there may be a delay between reality and the availability of data in the API.
      * Update Frequency : This endpoint is updated every 15 seconds.
      * Recommended Calls : 1 call per minute for the leagues, teams, fixtures who have at least one fixture in progress otherwise 1 call per day.
@@ -209,5 +209,170 @@ class FootballApi
     {
         $queryParams = ['last' => $last];
         return  FootballApi::callApi('fixtures?' . http_build_query($queryParams));
+    }
+
+
+    /**
+     * teamsStatistics
+     * Returns the statistics of a team in relation to a given competition and season. 
+     * It is possible to add the date parameter to calculate statistics from the beginning of the season to the given date. By default the API returns the statistics of all games played by the team for the competition and the season.
+     * Update Frequency : This endpoint is updated twice a day.
+     * Recommended Calls : 1 call per day for the teams who have at least one fixture during the day otherwise 1 call per week.
+     * Documentation : https://www.api-football.com/documentation-v3#tag/Teams/operation/get-teams-statistics
+     * @param  mixed $leagueId
+     * @param  mixed $season
+     * @param  mixed $teamId
+     * @param  mixed $date
+     * @return void
+     */
+    public static function teamsStatistics(int $leagueId, int $season, int $teamId, ?string $date)
+    {
+        $queryParams = ['league' => $leagueId, 'season' => $season, 'team' => $teamId];
+        if ($date)
+            $queryParams['date'] = $date;
+        return  FootballApi::callApi('teams/statistics?' . http_build_query($queryParams));
+    }
+
+
+    /**
+     * getTeamInformation
+     * The team id are unique in the API and teams keep it among all the leagues/cups in which they participate.
+     * Update Frequency : This endpoint is updated several times a week.
+     * Recommended Calls : 1 call per day.
+     * Documentation : https://www.api-football.com/documentation-v3#tag/Teams/operation/get-teams
+     *
+     * @param  mixed $teamId
+     * @return void
+     */
+    public static function team(int $teamId)
+    {
+        $queryParams = ['team' => $teamId];
+        return  FootballApi::callApi('teams?' . http_build_query($queryParams));
+    }
+
+
+    /**
+     * allTeams
+     * The team id are unique in the API and teams keep it among all the leagues/cups in which they participate.
+     * Update Frequency : This endpoint is updated several times a week.
+     * Recommended Calls : 1 call per day.
+     * Documentation : https://www.api-football.com/documentation-v3#tag/Teams/operation/get-teams
+     *
+     * @param  mixed $teamId
+     * @return void
+     */
+    public static function allTeams()
+    {
+        return  FootballApi::callApi('teams');
+    }
+
+    /**
+     * getTeamsByLeague
+     * The team id are unique in the API and teams keep it among all the leagues/cups in which they participate.
+     * Update Frequency : This endpoint is updated several times a week.
+     * Recommended Calls : 1 call per day.
+     * Documentation : https://www.api-football.com/documentation-v3#tag/Teams/operation/get-teams
+     * @param  mixed $leagueId
+     * @param  mixed $season
+     * @return void
+     */
+    public static function getTeamsByLeague(int $leagueId, int $season)
+    {
+        $queryParams = ['league' => $leagueId, 'season' => $season];
+        return  FootballApi::callApi('teams?' . http_build_query($queryParams));
+    }
+
+    /**
+     * getTeamsByCountry
+     * The team id are unique in the API and teams keep it among all the leagues/cups in which they participate.
+     * Update Frequency : This endpoint is updated several times a week.
+     * Recommended Calls : 1 call per day.
+     * Documentation : https://www.api-football.com/documentation-v3#tag/Teams/operation/get-teams
+     * @param  mixed $country
+     * @return void
+     */
+    public static function getTeamsByCountry(string $country)
+    {
+        $queryParams = ['country' => $country];
+        return  FootballApi::callApi('teams?' . http_build_query($queryParams));
+    }
+
+
+    /**
+     * getTeamsByCode
+     * The team id are unique in the API and teams keep it among all the leagues/cups in which they participate.
+     * Update Frequency : This endpoint is updated several times a week.
+     * Recommended Calls : 1 call per day.
+     * Documentation : https://www.api-football.com/documentation-v3#tag/Teams/operation/get-teams
+     * @param  mixed $code
+     * @return void
+     */
+    public static function getTeamsByCode(string $code)
+    {
+        $queryParams = ['code' => $code];
+        return  FootballApi::callApi('teams?' . http_build_query($queryParams));
+    }
+
+
+    /**
+     * getTeamsByVenu
+     * The team id are unique in the API and teams keep it among all the leagues/cups in which they participate.
+     * Update Frequency : This endpoint is updated several times a week.
+     * Recommended Calls : 1 call per day.
+     * Documentation : https://www.api-football.com/documentation-v3#tag/Teams/operation/get-teams
+     * @param  mixed $leagueId
+     * @param  mixed $season
+     * @return void
+     */
+    public static function getTeamsByVenu(int $venueId)
+    {
+        $queryParams = ['venue' => $venueId];
+        return  FootballApi::callApi('teams?' . http_build_query($queryParams));
+    }
+
+
+    /**
+     * getTeamsByCountry
+     * The team id are unique in the API and teams keep it among all the leagues/cups in which they participate.
+     * Update Frequency : This endpoint is updated several times a week.
+     * Recommended Calls : 1 call per day.
+     * Documentation : https://www.api-football.com/documentation-v3#tag/Teams/operation/get-teams
+     * @param  mixed $leagueId
+     * @param  mixed $season
+     * @return void
+     */
+    public static function searchTeam(string $searchKeyword)
+    {
+        $queryParams = ['search' => $searchKeyword];
+        return  FootballApi::callApi('teams?' . http_build_query($queryParams));
+    }
+
+
+
+    /**
+     * Get the list of seasons available for a team.
+     * This endpoint requires at least one parameter.
+     * Update Frequency : This endpoint is updated several times a week.
+     * Recommended Calls : 1 call per day.
+     * Documentation : https://www.api-football.com/documentation-v3#tag/Teams/operation/get-teams-seasons
+     *
+     * @param  mixed $teamId
+     * @return void
+     */
+    public static function getTeamSeason($teamId)
+    {
+        $queryParams = ['team' => $teamId];
+        return  FootballApi::callApi('teams/seasons?' . http_build_query($queryParams));
+    }
+    /**
+     * Get the list of countries available for the teams endpoint.
+     * Update Frequency : This endpoint is updated several times a week.
+     * Recommended Calls : 1 call per day.
+     * Documentation : https://www.api-football.com/documentation-v3#tag/Teams/operation/get-teams-countries
+     * @return void
+     */
+    public static function getTeamCountries()
+    {
+        return  FootballApi::callApi('teams/countries');
     }
 }
